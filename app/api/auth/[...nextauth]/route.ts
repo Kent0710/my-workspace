159d20afd3121d9007a72f66c1a73703aa869c10
Kent0710 <christiankent001@gmail.com>
2023-11-26 12:54:09 +0800

    auth config options file moved

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,28 +1,5 @@
 import NextAuth from "next-auth";
-import Auth0Provider from 'next-auth/providers/auth0'
-
-import { PrismaAdapter } from '@next-auth/prisma-adapter'
-import prisma from "@/lib/prisma"
-
-export const authOptions = {
-    adapter : PrismaAdapter(prisma),
-    providers : [
-        Auth0Provider({
-            clientId : process.env.AUTH0_CLIENT_ID || '',
-            clientSecret : process.env.AUTH0_CLIENT_SECRET || '',
-            issuer : process.env.AUTH0_ISSUER_BASE_URL || '',
-            authorization : {
-                params : {
-                    prompt : 'login'
-                }
-            }
-        })
-    ],
-    pages : {
-        signIn : "/signIn"
-    },
-    secret : process.env.AUTH0_SECRET || '',
-};
+import {authOptions} from '@/lib/auth'
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
