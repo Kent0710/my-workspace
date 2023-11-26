1a08a58a5572319bfe0c69be501abb1f5c2094db
Kent0710 <christiankent001@gmail.com>
2023-11-26 13:02:59 +0800

    auth commit

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,28 @@
-import NextAuth from "next-auth";
-import {authOptions} from '@/lib/auth'
+import NextAuth, {NextAuthOptions} from "next-auth";
+import Auth0Provider from 'next-auth/providers/auth0'
+
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import prisma from "@/lib/prisma"
+
+const authOptions : NextAuthOptions = {
+    adapter : PrismaAdapter(prisma),
+    providers : [
+        Auth0Provider({
+            clientId : process.env.AUTH0_CLIENT_ID || '',
+            clientSecret : process.env.AUTH0_CLIENT_SECRET || '',
+            issuer : process.env.AUTH0_ISSUER_BASE_URL || '',
+            authorization : {
+                params : {
+                    prompt : 'login'
+                }
+            }
+        })
+    ],
+    pages : {
+        signIn : "/signIn"
+    },
+    secret : process.env.AUTH0_SECRET || '',
+};
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
